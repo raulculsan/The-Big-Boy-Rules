d55585faeffaab99e754220bfd95c6eb947781fe
Raul Culsan <raulculsan@Rauls-MacBook-Pro.local>
2026-07-30 16:51:48 +0200

    Expand football news sources

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.BIG_BOY_CONFIG = {
   supabaseUrl: "https://nfvggpgeypkkqceivkaq.supabase.co",
   supabasePublishableKey: "sb_publishable_76a6wcA_Ow12tT9r_OlvWQ_MN7ALhfX",
   news: {
-    deportesFeed: "https://news.google.com/rss/search?q=f%C3%BAtbol%20OR%20LaLiga%20OR%20Champions%20OR%20fichajes%20OR%20%22selecci%C3%B3n%20espa%C3%B1ola%22&hl=es&gl=ES&ceid=ES%3Aes",
+    deportesFeed: "https://news.google.com/rss/search?q=f%C3%BAtbol%20OR%20LaLiga%20OR%20Champions%20OR%20fichajes%20OR%20%22selecci%C3%B3n%20espa%C3%B1ola%22%20OR%20%22Fabrizio%20Romano%22%20OR%20site%3Amarca.com%20OR%20site%3Aas.com%20OR%20site%3Amundodeportivo.com%20OR%20site%3Asport.es%20OR%20site%3Arelevo.com&hl=es&gl=ES&ceid=ES%3Aes",
     espanaFeed: "https://news.google.com/rss/search?q=Espa%C3%B1a&hl=es&gl=ES&ceid=ES%3Aes",
     mundoFeed: "https://news.google.com/rss/search?q=Internacional&hl=es&gl=ES&ceid=ES%3Aes"
   }
