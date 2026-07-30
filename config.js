650f0732123bef5ce84c789c498b18cd6d7d5a4b
Raul Culsan <raulculsan@Rauls-MacBook-Pro.local>
2026-07-30 16:57:07 +0200

    Add resilient sports news fallback

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ window.BIG_BOY_CONFIG = {
   supabasePublishableKey: "sb_publishable_76a6wcA_Ow12tT9r_OlvWQ_MN7ALhfX",
   news: {
     deportesFeed: "https://news.google.com/rss/search?q=f%C3%BAtbol%20OR%20%22Fabrizio%20Romano%22%20OR%20MARCA%20OR%20%22Diario%20AS%22%20OR%20%22Mundo%20Deportivo%22&hl=es&gl=ES&ceid=ES%3Aes",
+    deportesFallbackFeed: "https://news.google.com/rss/search?q=f%C3%BAtbol%20OR%20LaLiga%20OR%20Champions%20OR%20fichajes%20OR%20%22selecci%C3%B3n%20espa%C3%B1ola%22&hl=es&gl=ES&ceid=ES%3Aes",
     espanaFeed: "https://news.google.com/rss/search?q=Espa%C3%B1a&hl=es&gl=ES&ceid=ES%3Aes",
     mundoFeed: "https://news.google.com/rss/search?q=Internacional&hl=es&gl=ES&ceid=ES%3Aes"
   }
