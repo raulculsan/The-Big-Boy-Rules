c38248a04aa222e31caeb3f2a9c4de8506994267
raulculsan <raulculsangonz@gmail.com>
2026-08-05 18:10:07 +0200

    Refresh PWA cache v77

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "big-boy-rules-v76";
+const CACHE_NAME = "big-boy-rules-v77";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260805-76",
-  "./app.js?v=20260805-76",
+  "./styles.css?v=20260805-77",
+  "./app.js?v=20260805-77",
   "./config.js?v=20260805-59",
-  "./manifest.webmanifest?v=20260805-76",
+  "./manifest.webmanifest?v=20260805-77",
   "./icons/icon.svg",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
